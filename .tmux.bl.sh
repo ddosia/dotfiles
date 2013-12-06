f04a7c0d134de41e4bfa04ac8cdbf03672d642c2
Churikov Daniil <ddosia@gmail.com>
2013-12-06 12:54:27 +0400

    tmux status bar changes

--- .tmux.bl.sh
+++ .tmux.bl.sh
@@ -5,7 +5,7 @@ tot_bl=$(cat /sys/class/power_supply/BAT0/energy_full)
 bl=`echo $(( $cur_bl * 100 / $tot_bl )) | bc -l | cut -d '.' -f 1`
 
 if [[ $bl -lt 20 ]]; then
-    echo "#[fg=red][$bl%]#[default]"
+    echo "[⚡ #[fg=red]$bl#[default]%]"
 else
-    echo "[$bl%]"
+    echo "[⚡ $bl%]"
 fi
