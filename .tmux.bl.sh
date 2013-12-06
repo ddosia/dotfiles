7f0cd5a8c7640b432719639de7120fe20f4de8c7
Churikov Daniil <ddosia@gmail.com>
2013-12-06 12:41:07 +0400

    Add battery life to tmux status bar

---
+++ .tmux.bl.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+cur_bl=$(cat /sys/class/power_supply/BAT0/energy_now)
+tot_bl=$(cat /sys/class/power_supply/BAT0/energy_full)
+bl=`echo $(( $cur_bl * 100 / $tot_bl )) | bc -l | cut -d '.' -f 1`
+
+if [[ $bl -lt 20 ]]; then
+    echo "#[fg=red][$bl%]#[default]"
+else
+    echo "[$bl%]"
+fi
