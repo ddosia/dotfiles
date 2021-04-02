45bc60003d08df27b8ba0d376682b09dea3e99e3
Daniil Churikov <ddosia@gmail.com>
2021-04-02 10:38:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,37 +1,58 @@
 #!/bin/bash
 
-## SUBMODULES
-echo "Initializing submodules"
-git submodule update --init
-for s in $( git config --file .gitmodules --name-only --get-regexp "path$" | sed 's/submodule.\(.*\).path/\1/' ); do
-    mkdir -p "$(dirname "${HOME}/${s}")"
-    ln -frTvs $s ~/$s
-done;
-echo ""
-
 function ubuntu_install {
     sudo apt-get update;
+    echo ""
+    ubuntu_dev_essentials_install
     echo ""
     ubuntu_pyenv_install
     echo ""
     sudo apt install awscli
     echo ""
-    sudo apt install ubuntu_terraform_install
+    ubuntu_terraform_install
 }
 
 function mac_install {
     xcode-select --install
     brew update
+    mac_dev_essentials_install
     echo ""
     mac_pyenv_install
     echo ""
 }
 
+################
+### dev essentials
+function ubuntu_dev_essentials_install {
+    sudo apt install build-essential autoconf automake gdb libffi-dev zlib1g-dev libssl-dev
+    sudo apt install git tmux vim htop colordiff
+}
+
+function mac_dev_essentials_install {
+    brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
+    brew install git tmux vim bash bash-completion htop wget curl colordiff
+    brew install autoconf automake libtool
+}
+###
+################
+
+
+################
+### pyenv
 function ubuntu_pyenv_install {
     echo "Please read pyenv installation notes on https://github.com/pyenv/pyenv"
     sudo apt install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 }
 
+function mac_pyenv_install {
+    echo "Please read pyenv installation notes on https://github.com/pyenv/pyenv"
+    brew install openssl readline sqlite3 xz zlib
+}
+###
+################
+
+################
+### terraform
 function ubuntu_terraform_install {
     echo "Please read terraform installation notes on https://learn.hashicorp.com/tutorials/terraform/install-cli"
     sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
@@ -39,14 +60,24 @@ function ubuntu_terraform_install {
     sudo apt-get update && sudo apt-get install terraform
     terraform -install-autocomplete
 }
-
-function mac_pyenv_install {
-    echo "Please read pyenv installation notes on https://github.com/pyenv/pyenv"
-    brew install openssl readline sqlite3 xz zlib
-}
+###
+################
 
 
 case "$(uname -s)" in
     Linux*) ubuntu_install;;
     Darwin*) mac_install;;
 esac
+
+
+################
+### SUBMODULES
+echo "Initializing submodules"
+git submodule update --init
+for s in $( git config --file .gitmodules --name-only --get-regexp "path$" | sed 's/submodule.\(.*\).path/\1/' ); do
+    mkdir -p "$(dirname "${HOME}/${s}")"
+    ln -frTvs $s ~/$s
+done;
+echo ""
+###
+################
