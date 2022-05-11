b6bb847cbd99d536f55c05d628fa03e8053b9084
Daniil Churikov <ddosia@gmail.com>
2022-05-11 12:00:21 +0100

    Install pipenv

--- install.sh
+++ install.sh
@@ -82,6 +82,7 @@ python -m pip install -U pip
 python -m pip install -U matplotlib
 python -m pip install -U pandas
 python -m pip install -U jupyter
+python -m pip install -U pipenv
 ###
 ################
 
